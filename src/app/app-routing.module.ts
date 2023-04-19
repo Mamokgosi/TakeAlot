be512b0c73b27b86cc4df0e2167a127a53d9cb17
DA MAC M1 149 <damacm1149@DAs-MacBook-Air-M1-149.local>
2023-04-19 10:04:53 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductComponent } from './pages/product/product.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'product/:productId', component: ProductComponent},
+//{path:'**', redirectTo: '', pathMatch: 'full'}, It will direct you to home page if you type anything that doesn't exist
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ProductCartComponent } from './components/product-cart/product-cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductComponent } from './pages/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    HeaderComponent,
+    ProductCartComponent,
+    FooterComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
